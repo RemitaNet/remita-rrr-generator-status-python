cb3df4ebe8275b1f5b3d99d1aa6b632add37eb34
Iyare I. Diagboya <toolz.skillz@gmail.com>
2021-09-23 09:19:21 +0100

    Genesis push

---
+++ CheckRRRStatus.py
@@ -0,0 +1,21 @@
+import requests
+import hashlib
+
+
+def sha512(input):
+    hashed_input = hashlib.sha512(input.encode('utf-8'))
+    hex_dig = hashed_input.hexdigest()
+    return hex_dig
+
+
+merchantId = "2547916"
+apiKey = "1946"
+rrr = "240008240803"
+
+apiHash = sha512(rrr + apiKey + merchantId)
+print(apiHash)
+
+checkStatus = 'https://remitademo.net/remita/ecomm/' + merchantId + "/" + rrr + "/" + apiHash + "/status.reg"
+print(checkStatus)
+r = requests.get(checkStatus)
+print(r.text)
---
+++ GenerateRRR.py
@@ -0,0 +1,44 @@
+import requests
+import hashlib
+import random
+
+
+def sha512(inputString):
+    hashed_input = hashlib.sha512(inputString.encode('utf-8'))
+    hex_dig = hashed_input.hexdigest()
+    return hex_dig
+
+
+def formatResponse(input):
+    response = input.text
+    return response[7:87]
+
+
+url = "https://remitademo.net/remita/exapp/api/v1/send/api/echannelsvc/merchant/api/paymentinit"
+merchantId = "2547916"
+apiKey = "1946"
+serviceTypeId = "4430731"
+orderId = str(random.random() * 1000000000)
+print(orderId)
+totalAmount = "20000"
+
+apiHash = sha512(merchantId + serviceTypeId + orderId + totalAmount + apiKey)
+print(apiHash)
+
+headers = {'Content-Type': 'application/json',
+           'Authorization': 'remitaConsumerKey=' + merchantId + ',remitaConsumerToken=' + apiHash
+           }
+
+payload = {
+    'serviceTypeId': serviceTypeId,
+    'amount': totalAmount,
+    'orderId': orderId,
+    'payerName': 'Michael Alozie',
+    'payerEmail': 'alozie@systemspecs.com.ng',
+    'payerPhone': '09062067384',
+    'description': 'Payment for Donation 3'
+}
+
+response = requests.post(url, headers=headers, json=payload)
+response = formatResponse(response)
+print(response)
